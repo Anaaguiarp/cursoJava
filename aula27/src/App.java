94dcd9a1bdd6d9ae1337769fe783bf33e5405c35
Ana Carolina A Pereira <anacarolinaaguiarp@gmail.com>
2025-04-03 17:22:48 -0300

    Add files via upload

---
+++ aula27/src/App.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        int x;
+        String s1, s2, s3;
+
+        x = sc.nextInt();
+        sc.nextLine(); //limpando buffer de leitura
+        s1 = sc.nextLine();
+        s2 = sc.nextLine();
+        s3 = sc.nextLine();
+        //next().charAt(0) para registrar apenas 1 caractere
+    }
+}
